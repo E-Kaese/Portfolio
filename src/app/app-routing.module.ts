271d9a9773caa12f8d1b92fbca08efc8cdc20d40
E-Kaese <ernstkaese@gmail.com>
2018-07-24 11:39:59 +0200

    Fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,12 @@ import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MetaGuard } from '@ngx-meta/core';
 
 const routes: Routes = [
-    {
-        path: '',
-        canActivateChild: [MetaGuard],
-        children: [
-            { path: '', redirectTo: '/main', pathMatch: 'full' },
-            { path: 'main', component: ProjectsComponent },
-            { path: 'skills', component: SkillsComponent },
-            { path: 'contact', component: ContactComponent }
-        ]
-    }
+    { path: '', redirectTo: '/main', pathMatch: 'full' },
+    { path: 'main', component: ProjectsComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
@@ -23,4 +16,4 @@ const routes: Routes = [
     exports: [RouterModule]
 })
 
-export class AppRoutingModule { }
+export class AppRoutingModule {}
